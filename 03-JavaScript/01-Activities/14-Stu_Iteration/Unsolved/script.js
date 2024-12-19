fa952e6b58bb14b9ef9be4674793077b2e51b805
Edward O <edwardoreilly0906@gmail.com>
2024-12-18 21:41:56 -0600

    Week 2 Java activities

--- 03-JavaScript/01-Activities/14-Stu_Iteration/Unsolved/script.js
+++ 03-JavaScript/01-Activities/14-Stu_Iteration/Unsolved/script.js
@@ -1,1 +1,11 @@
 // WRITE YOUR CODE BELOW
+const students = ['Jon', 'Jacob', 'Jane', 'Julie'];
+
+
+for (let i = 0; i < students.length; i++) {
+    console.log(` Great to see you, ${students[i]}`);
+  }
+
+  for (const names of students) {
+    console.log(names);
+  }
